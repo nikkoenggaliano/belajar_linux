d9b093c166c89f40e75ce59079d2f5ceb362b0ec
nikko <nikkoenggaliano@gmail.com>
2019-08-19 19:21:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
+<?PHP
+if(!empty($_FILES['uploaded_file'])){
+
+$path = __DIR__.'/uploaded/';
+
+if(!is_dir($path)){
+        @mkdir($path);
+        @chmod($path, 0000);
+}
+        $path = $path . basename( $_FILES['uploaded_file']['name']);
+
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']).
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, The error cause permission for folder uploaded";
+    }
+  }
+?>
